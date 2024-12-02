af2c2902aa2e03f81da8f1a0e3ab3e6c0b462c75
mengaww2ya <mengaww2ya@gmail.com>
2024-12-02 23:44:40 +0300

    un necesary import is removed from hompage

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
-import RootNavigator from '../navigator/RootNavigator.js';
-import Homepage from '../screans/homepage.js'
-import { ScrollView } from 'react-native';
 import AuthicStackNavig from '../navigator/authenticatior.js'
-import { NavigationContainer } from '@react-navigation/native';
-
 export default function App() {
   return (
       <SafeAreaView style={styles.safeContainer}>
