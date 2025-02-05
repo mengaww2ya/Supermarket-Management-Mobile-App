fb0fe711eb5647fdcd16943cc04a49fca700c34c
mengaww2ya <mengaww2ya@gmail.com>
2025-02-05 15:57:43 +0300

    modal on footer

--- src/subscrean/foter.js
+++ src/subscrean/foter.js
@@ -1,8 +1,20 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import { Icon } from "react-native-elements";
-
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Modal,
+  TouchableOpacity,
+  Text,
+  Image,
+  useWindowDimensions,
+} from "react-native";
+import { colors, Icon } from "react-native-elements";
+const screenwidth = useWindowDimensions().width;
+const screenheight = useWindowDimensions().height;
 export default function Footer({ navigation }) {
+  const [viewProfile, setviewProfile] = useState(false);
+  const [viewFvorite, setviewFvorte]=useState(false)
+   
   return (
     <View style={styles.footerContainer}>
       {/* Home */}
@@ -21,8 +33,7 @@ export default function Footer({ navigation }) {
         color="#517fa4"
         size={30}
         onPress={() => {
-        
-           alert("Hey! this button is not functional right now.", "ok");
+          alert("Hey! this button is not functional right now.", "ok");
         }}
       />
 
@@ -34,7 +45,7 @@ export default function Footer({ navigation }) {
         size={30}
         onPress={() => {
           // navigation.navigate("Orders")
-           alert("Hey! this button is not functional right now.", "ok");
+          alert("Hey! this button is not functional right now.", "ok");
         }}
       />
 
@@ -46,7 +57,7 @@ export default function Footer({ navigation }) {
         size={30}
         onPress={() => {
           // navigation.navigate("Favorites");
-           alert("Hey! this button is not functional right now.", "ok");
+          setviewFvorte(true)
         }}
       />
 
@@ -57,10 +68,56 @@ export default function Footer({ navigation }) {
         color="#517fa4"
         size={30}
         onPress={() => {
-          // navigation.navigate("Profile"); 
-          alert("Hey! this button is not functional right now.", "ok")
+          // navigation.navigate("Profile");
+          setviewProfile(true);
         }}
       />
+      <Modal
+        visible={viewProfile}
+        onRequestClose={() => {
+          setviewProfile(false);
+        }}
+        animationType="slide"
+        presentationStyle="fullScreen"
+      >
+        <View style={styles.modalview}>
+          <Text style={styles.titltxt}>Profile setting</Text>
+          <View style={styles.container}>
+            <Image />
+            <TouchableOpacity
+              onPress={() => {
+                setviewProfile(false);
+              }}
+              style={styles.modalclosbtn}
+            >
+              <Text style={styles.btntxt}>close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+      <Modal
+        visible={viewFvorite}
+        onRequestClose={() => {
+          setviewFvorte(false);
+        }}
+        animationType="slide"
+        presentationStyle="fullScreen"
+      >
+        <View style={styles.modalview}>
+          <Text style={styles.titltxt}>All favorite products</Text>
+          <View style={styles.container}>
+            <Image />
+            <TouchableOpacity
+              onPress={() => {
+                setviewFvorte(false);
+              }}
+              style={styles.modalclosbtn}
+            >
+              <Text style={styles.btntxt}>close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -71,5 +128,28 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     paddingVertical: 10,
+  },
+  modalview: {
+    backgroundColor: "white",
+  },
+  container:{
+  },
+  titltxt: {
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  modalclosbtn: {
+    backgroundColor: colors.grey4,
+    alignContent: "center",
+    borderRadius: 5,
+    borderWidth: 1,
+    width: screenwidth * 0.3,
+    alignSelf: "center",
+  },
+  btntxt: {
+    fontSize: 18,
+    textAlign: "center",
+    fontWeight: "bold",
   },
 });
