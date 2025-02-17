6643205ab8570828933731ab703819f0143d88dc
mengaww2ya <mengaww2ya@gmail.com>
2025-02-17 17:17:11 +0300

    native wind installed and configured

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, View } from "react-native";
+import "../../global.css";
 import AuthicStackNavig from "../navigator/stackNavigator.js";
 export default function App() {
   return (
